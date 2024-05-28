8ee90a32b5e122314bfcc5160c7ca4593ee43157
kpc-simone <kpc.simone@gmail.com>
2024-05-28 14:36:10 -0600

    prioritize visualization in search path

--- scripts/plot_all_trial_paths.py
+++ scripts/plot_all_trial_paths.py
@@ -7,7 +7,7 @@ import argparse
 import math
 
 import sys,os
-sys.path.append(os.path.join(os.path.dirname(__file__),'../src'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../src'))
 from visualization import behavior_colors, draw_box
 
 ### SCRIPT PARAMETERS ###

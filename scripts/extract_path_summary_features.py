afeee70d37a6e0c07fdaf8b8027ed44e2b239617
kpc-simone <kpc.simone@gmail.com>
2024-10-25 09:29:05 -0400

    add script to extract summary features from path data

--- scripts/extract_path_summary_features.py
+++ scripts/extract_path_summary_features.py
@@ -1,4 +1,4 @@
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename,askdirectory, asksaveasfile
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 from scipy import signal
@@ -7,56 +7,83 @@ import numpy as np
 import sys,os
 import math
 
-trajectories_folder = '../annotated-paths'
-FPS = 20
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--FPS', default = 30, type = int)
+parser.add_argument('--outcome_col', default = 'outcome', type = str )
+parser.add_argument('--run_abs_col', default = 'beam-break-rel', type = str)
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    FPS = args.FPS
+    outcome_col = args.outcome_col
+    run_abs_col = args.run_abs_col
+        
+    print('Select csv file with behavior and stimulus timings.')    
+    ktdf = pd.read_csv(askopenfilename(title = "Select csv file with behavior and stimulus timings"))
     
-print('select csv file with behavior and stimulus timings')    
-ktdf = pd.read_csv(askopenfilename())
-ktdf = ktdf[ktdf['defensive strategy'] != 'mistrial']
-ktdf = ktdf[ktdf['day'] == 1]
+    print('Select folder containing annotated paths.')
+    trajectories_folder = askdirectory(title = "Select folder containing annotated paths")
 
-fdf = pd.DataFrame( columns = ['phenotype','sex','animal','trial','outcome','latency','c-start','c-end','dist-euclid','dist-total','curvature','speed-mean','speed-peak'])
-for trajectory_file in os.listdir(trajectories_folder):
-    traj_df = pd.read_csv(os.path.join(trajectories_folder,trajectory_file))
+    ktdf = ktdf[ktdf[outcome_col] != 'mistrial']
+
+    data = []
+    print('Extracting summary features from path data ...')
+    for trajectory_file in os.listdir(trajectories_folder):
         
-    trajectory_info = trajectory_file[:-4].split('-')
-    phenotype = trajectory_info[0]
-    sex = trajectory_info[1]
-    animal = trajectory_info[2]
-    day = trajectory_info[3]
-    trial = int(trajectory_info[4][1:])
-    outcome = trajectory_info[5]
+        trajectory_info = trajectory_file[:-4].split('-')
+        group = trajectory_info[0].lower()
+        animal = trajectory_info[2]
+        trial = int(trajectory_info[4][1:])
+        #print(trajectory_file, group, animal, trial)
+        
+        mask = (ktdf['group'] == group) & (ktdf['animal'] == animal) & (ktdf['trial'] == trial)
+        trial_df = ktdf[ mask ].reset_index(drop=True)
+        
+        if len(trial_df) == 0:
+            print(f'WARNING: entry missing for group {group}, animal {animal}, trial-{trial}')
+        else:
+            # extract features
+            traj_df = pd.read_csv(os.path.join(trajectories_folder,trajectory_file))
+            c_start = (traj_df['c-xpos'].iloc[0],traj_df['c-ypos'].iloc[0])
+            c_end = (traj_df['c-xpos'].iloc[-1],traj_df['c-ypos'].iloc[-1])
+            
+            dist_e = np.sqrt( (c_end[0] - c_start[0])**2 + (c_end[1] - c_start[1])**2 )
+            dist_t = traj_df['c-speed'].cumsum().max() / FPS
+            
+            run_latency = 'N/A'
+            if trial_df[run_abs_col] is not None:
+                run_latency = float(trial_df[run_abs_col]) - float(trial_df['shadowON-abs'])
+                # TODO add a warning if marked as freeze but beam broken
+            
+            data.append({
+            # basic identifying features
+            'group'         : group,
+            'animal'        : animal,
+            'trial'         : trial,
+            
+            # additional identifying features
+            'outcome'       : trial_df[outcome_col],
+            
+            # characterizing features
+            'latency'       : run_latency,
+            'c-start'       : c_start,
+            'c-end'         : c_end,
+            'dist-euclid'   : dist_e,
+            'dist-total'    : dist_t,
+            'curvature'     : dist_t / dist_e ,
+            'speed-mean'    : traj_df['c-speed'].mean(),
+            'speed-peak'    : traj_df['c-speed'].max(),
+            'acc-peak'      : traj_df['c-scacc'].max(),
+            'xacc-peak'      : traj_df['c-xacc'].max(),
+            'yacc-peak'      : traj_df['c-yacc'].max(),
+            })
+    fdf = pd.DataFrame( data )
+    print('Summary features extracted. Head/Tail of dataframe: ')
+    print(fdf.head())    
+    print(fdf.tail())    
     
-    if (outcome == 'escape') or (outcome == 'panic'):
-        trial_df = ktdf[ (ktdf['animal'] == animal) & (ktdf['trial'] == trial)]
-        
-        c_start = (traj_df['c-xpos'].iloc[0],traj_df['c-ypos'].iloc[0])
-        c_end = (traj_df['c-xpos'].iloc[-1],traj_df['c-ypos'].iloc[-1])
-        
-        dist_e = np.sqrt( (c_end[0] - c_start[0])**2 + (c_end[1] - c_start[1])**2 )
-        dist_t = traj_df['c-speed'].cumsum().max() / FPS
-        #print(dist_e)
-        
-        fdf = fdf.append( {
-        'phenotype'     : phenotype,
-        'sex'           : sex,
-        'animal'        : animal,
-        'trial'         : trial,
-        'outcome'       : outcome,
-        'latency'       : float(trial_df['run-abs']) - float(trial_df['shadowON-abs']),
-        'c-start'       : c_start,
-        'c-end'         : c_end,
-        'dist-euclid'   : dist_e,
-        'dist-total'    : dist_t,
-        'curvature'     : dist_t / dist_e ,
-        'speed-mean'    : traj_df['c-speed'].mean(),
-        'speed-peak'    : traj_df['c-speed'].max(),
-        'acc-peak'      : traj_df['c-scacc'].max(),
-        'xacc-peak'      : traj_df['c-xacc'].max(),
-        'yacc-peak'      : traj_df['c-yacc'].max(),
-        }, ignore_index = True)
-
-print(fdf.head())    
-print(fdf.tail())    
-
-fdf.to_csv('extracts/escape-path-features.csv')
+    print('Select location to save output data.')
+    fdf.to_csv(asksaveasfile(title='Select location to save output data.'))
